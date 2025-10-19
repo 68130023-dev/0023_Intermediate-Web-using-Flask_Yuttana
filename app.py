ad1167c217a3e359ffd401bedf7af2b00443d0ad
68130023 <68130023@email.com>
2025-10-19 17:39:35 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+def abc(p1, p2):
+    return p1 + p2
+
+def abc1(p1, p2):
+    return int(p1) + int(p2)
+
+@app.route("/")
+def home():
+    return render_template("index.html", title="หน้าแรก | Flask Starter")
+
+
+@app.route("/Tech")
+def Tech():
+    return render_template("Tech.html", title="เกี่ยวกับเทคโนโลยี | Flask Starter")
+
+@app.route("/MyID")
+def MyID():
+    return render_template("MyID.html", title="MyID | Flask Starter")
+
+@app.route("/draw")
+def draw():
+    return render_template("draw.html", title="draw | Flask Starter")
+
+@app.route('/concat/<para1>/<para2>')
+def concat(para1, para2):
+    result = abc(para1, para2)
+    return f"The result of concatenate between '{para1}' and '{para2}' is '{result}'"
+
+@app.route('/sum/<para3>/<para4>')
+def sumt(para3, para4):
+    sum_result = abc1(para3, para4)
+    return f"The result of sum between '{para3}' and '{para4}' is '{sum_result}'"
+
+if __name__ == "__main__":
+   
+    app.run(host='0.0.0.0',debug=True)
---
+++ static/js/app.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("helloBtn");
+  if (btn) {
+    btn.addEventListener("click", () => alert("ชื่อ: Yuttana Emyong\nรหัสนักศึกษา: 68130023🎉"));
+  }
+});
